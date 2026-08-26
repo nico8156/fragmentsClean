22334143ad2cc5e90368e9e57a02b29afd8f3461
nico8156 <119879294+nico8156@users.noreply.github.com>
2026-08-26 19:51:14 +0200

    fix: make staging backend deployable

--- src/main/java/com/nm/fragmentsclean/coffeeContext/read/adapters/primary/springboot/admin/AdminCoffeesReadController.java
+++ src/main/java/com/nm/fragmentsclean/coffeeContext/read/adapters/primary/springboot/admin/AdminCoffeesReadController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,6 +40,7 @@ public class AdminCoffeesReadController {
 	private final CoffeePhotoUriResolver photoUriResolver;
 	private final AdminAuditRecorder adminAuditRecorder;
 
+	@Autowired
 	public AdminCoffeesReadController(CommandBus commandBus,
 			QueryBus queryBus,
 			CoffeePhotoProjectionRepository photoProjectionRepository,
