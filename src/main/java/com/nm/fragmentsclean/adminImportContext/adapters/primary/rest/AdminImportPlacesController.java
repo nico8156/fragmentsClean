b64578d7e4710870760271882d583d8b09ad3ea7
nico8156 <119879294+nico8156@users.noreply.github.com>
2026-08-26 19:56:05 +0200

    fix: select admin controller constructors explicitly

--- src/main/java/com/nm/fragmentsclean/adminImportContext/adapters/primary/rest/AdminImportPlacesController.java
+++ src/main/java/com/nm/fragmentsclean/adminImportContext/adapters/primary/rest/AdminImportPlacesController.java
@@ -6,6 +6,7 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,7 @@ public class AdminImportPlacesController {
 	private final RecordAdminAudit recordAdminAudit;
 	private final DateTimeProvider dateTimeProvider;
 
+	@Autowired
 	public AdminImportPlacesController(SearchGooglePlacesForCoffee searchGooglePlacesForCoffee,
 			PreviewGooglePlaceCoffee previewGooglePlaceCoffee,
 			ImportGooglePlaceCoffee importGooglePlaceCoffee,
